47583d6e98a4c8536d346442126375eb3ca6b68f
testDanilo <hekireki98@gmail.com>
2021-04-22 12:12:51 -0300

    Clases, metodos y atributos

--- src/ar/edu/unlam/cerradura/Cerradura.java
+++ src/ar/edu/unlam/cerradura/Cerradura.java
@@ -1,5 +1,31 @@
+//Cerradura	
+//	+String ContraseñaVerdadera
+//	
+//	*abrirCerradura
+//	*cerrarCerradura
+
 package ar.edu.unlam.cerradura;
 
 public class Cerradura {
+	private String contraseñaVerdadera;
+	private Boolean estadoCerradura;
 
+	public Cerradura(String contraseñaVerdadera) {
+		this.contraseñaVerdadera = contraseñaVerdadera;
+		this.estadoCerradura = false;
+	}
+	
+	public Boolean verSiLaContraseñaIngresadaEsCorrecta(String contraseñaIngresada) {
+		if(contraseñaVerdadera.equals(contraseñaIngresada)) {
+			this.estadoCerradura = true;
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void cerrarCerradura() {
+		this.estadoCerradura = false;
+	}
+	
 }
--- src/ar/edu/unlam/cerradura/Puerta.java
+++ src/ar/edu/unlam/cerradura/Puerta.java
@@ -1,5 +1,40 @@
+//Puerta
+//	+Boolean EstadoPuerta
+//	+Cerradura c1
+//	+Cerradura c2
+//	+Cerradura c3
+//
+//	*estaLaPuertaAbierta
+//	*abrirPuerta(c1,c2,c3)
+//	*cerrarPuerta(bloquea la puerta)
+	
 package ar.edu.unlam.cerradura;
 
 public class Puerta {
+	private Cerradura c1,c2,c3;
+	private Boolean estadoPuerta;
+	
+	public Puerta(Cerradura c1, Cerradura c2, Cerradura c3) {
+		this.c1 = c1;
+		this.c2 = c2;
+		this.c3 = c3;
+		this.estadoPuerta = false;
+	}
+	
+	public Boolean estaLaPuertaAbierta() {
+		return this.estadoPuerta;
+	}
+	
+	public void abrirPuerta(Cerradura c1, Cerradura c2, Cerradura c3) {
+		this.estadoPuerta = true;
+	}
+	
+	public void cerrarPuerta() {
+		this.estadoPuerta = false;
+		c1.cerrarCerradura();
+		c2.cerrarCerradura();
+		c3.cerrarCerradura();
+	}
 
+	
 }
